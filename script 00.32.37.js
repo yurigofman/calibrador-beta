d8bcd396b3d671d5b1ad685769f6fa9d46c6c1d6
yurigofman <154927917+yurigofman@users.noreply.github.com>
2023-12-29 23:49:20 -0300

    Add files via upload

---
+++ script 00.32.37.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('threejs-canvas').appendChild(renderer.domElement);
+    camera.position.z = 5;
+
+    const esferas = criarEsferas();
+    esferas.forEach(esfera => scene.add(esfera));
+
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+
+    window.addEventListener('click', (event) => {
+        mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+        mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+        raycaster.setFromCamera(mouse, camera);
+        const intersects = raycaster.intersectObjects(esferas);
+
+        if (intersects.length > 0) {
+            const esferaClicada = intersects[0].object;
+            abrirFormulario(esferaClicada.name);
+        }
+    });
+
+    animate();
+});
+
+function criarEsferas() {
+    const geometria = new THREE.SphereGeometry(1, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.5 });
+
+    const esferaEmocional = new THREE.Mesh(geometria, material);
+    esferaEmocional.position.set(-1.5, 0, 0);
+    esferaEmocional.name = 'formularioEmocional';
+
+    const esferaSaude = new THREE.Mesh(geometria, material);
+    esferaSaude.position.set(1.5, 0, 0);
+    esferaSaude.name = 'formularioSaude';
+
+    const esferaDesenvolvimento = new THREE.Mesh(geometria, material);
+    esferaDesenvolvimento.position.set(0, 1.5, 0);
+    esferaDesenvolvimento.name = 'formularioDesenvolvimento';
+
+    return [esferaEmocional, esferaSaude, esferaDesenvolvimento];
+}
+
+function abrirFormulario(nomeFormulario) {
+    const formulario = document.getElementById(nomeFormulario);
+    if (formulario) {
+        formulario.style.display = 'block';
+    }
+}
+
+function calcularMedia(formularioId) {
+    const formulario = document.getElementById(formularioId);
+    let total = 0, count = 0;
+    formulario.querySelectorAll('input[type=range]').forEach(input => {
+        total += parseInt(input.value, 10);
+        count++;
+    });
+    const media = total / count;
+    alert('Média para ' + formularioId + ': ' + media.toFixed(2));
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+   
